b97e1a5473e6629c529e459afca6e4edc065182f
unknown <anel1989@googlemail.com>
2016-11-27 20:10:51 +0000

    Added a new service for signup

--- services/api.js
+++ services/api.js
@@ -11,107 +11,148 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-router.get('/api/login/',function(req,res){
+router.put('/api/login/', function(req, res) {
 
-  var username = req.query.username;
-  var password = req.query.password;
+  var username = req.body.username;
+  var password = req.body.password;
   console.log('username', username);
   console.log('password', password);
 
-  connection.query("SELECT * FROM users WHERE user_name='"+username+"' AND user_password='"+password+"'", function(err, rows, fields) {
+  connection.query("SELECT * FROM users WHERE user_name='" + username + "' AND user_password='" + password + "'", function(err, rows, fields) {
     if (err) {
-        console.error(err);
-        res.json(["Query error"]);
-        return;
+      console.error(err);
+      res.json(["Query error"]);
+      return;
     }
     console.log('The database users contains: ', rows);
-    if(rows.length > 0){
-        res.json(rows);
+    if (rows.length > 0) {
+      res.json(rows);
     } else {
-        res.status(403);
-        res.json(["Invalid login details"]);
+      res.status(403);
+      res.json(["Invalid login details"]);
     }
   });
 
 });
 
-router.get('/api/archscores/',function(req,res){
+router.get('/api/archscores/', function(req, res) {
 
   console.log('Get request received');
 
   var scoreId = req.query.scoreId;
   var userId = req.query.userId;
-  if (scoreId && userId){
-    connection.query('SELECT * FROM arch_scores WHERE score_id='+scoreId+' AND user_id='+userId, function(err, rows, fields) {
-    if (err) {
+  if (scoreId && userId) {
+    connection.query('SELECT * FROM arch_scores WHERE score_id=' + scoreId + ' AND user_id=' + userId, function(err, rows, fields) {
+      if (err) {
         console.error(err);
         res.json(["Query error"]);
         return;
-    }
-    console.log('The database users contains: ', rows);
+      }
+      console.log('The database users contains: ', rows);
 
-    if(rows.length > 0){
+      if (rows.length > 0) {
         res.json(rows);
-    } else {
+      } else {
         res.status(204);
         res.json(["No scores found"]);
-    }
-  });
-  } else if (scoreId){
-      connection.query('SELECT * FROM arch_scores WHERE score_id='+scoreId, function(err, rows, fields) {
-        if (err) {
-            console.error(err);
-            res.json(["Query error"]);
-            return;
-        }
-        console.log('The database users contains: ', rows);
+      }
+    });
+  } else if (scoreId) {
+    connection.query('SELECT * FROM arch_scores WHERE score_id=' + scoreId, function(err, rows, fields) {
+      if (err) {
+        console.error(err);
+        res.json(["Query error"]);
+        return;
+      }
+      console.log('The database users contains: ', rows);
 
-        if(rows.length > 0){
-            res.json(rows);
-        } else {
-            res.status(204);
-            res.json(["No scores found"]);
-        }
-      });
-  } else if (userId){
-      connection.query('SELECT * FROM arch_scores WHERE user_id='+userId, function(err, rows, fields) {
-        if (err) {
-            console.error(err);
-            res.json(["Query error"]);
-            return;
-        }
-        console.log('The database users contains: ', rows);
+      if (rows.length > 0) {
+        res.json(rows);
+      } else {
+        res.status(204);
+        res.json(["No scores found"]);
+      }
+    });
+  } else if (userId) {
+    connection.query('SELECT * FROM arch_scores WHERE user_id=' + userId, function(err, rows, fields) {
+      if (err) {
+        console.error(err);
+        res.json(["Query error"]);
+        return;
+      }
+      console.log('The database users contains: ', rows);
 
-        if(rows.length > 0){
-            res.json(rows);
-            res.json(["Query error"]);
-        } else {
-            res.status(204);
-            res.json(["No scores found"]);
-        }
-      });
+      if (rows.length > 0) {
+        res.json(rows);
+        res.json(["Query error"]);
+      } else {
+        res.status(204);
+        res.json(["No scores found"]);
+      }
+    });
   } else {
-     res.status(404);
-     res.json([]);
-     return;
+    res.status(404);
+    res.json([]);
+    return;
   }
 
 });
 
-router.put('/api/users',function(req,res){
+router.post('/api/signup/', function(req, res) {
 
-    console.log('Put request received');
+  var username = req.body.username;
+  var password = req.body.password;
+  var email = req.body.email;
+  console.log('email', email);
+  console.log('username', username);
+  console.log('password', password);
 
-    connection.query('INSERT INTO users (user_name, user_password, user_email) VALUES ("test", "password", "aaa@mail.com")', function(err, rows, fields) {
-        if (err) {
+  if (username && password && email) {
+    console.log('1');
+    connection.query("SELECT * FROM users WHERE user_email='" + email + "'", function(err, rows, fields) {
+      if (err) {
+        console.log('2');
+        console.error(err);
+        res.json(["Query error"]);
+        return;
+      } else if (rows.length > 0) {
+        console.log('3');
+        res.status(403);
+        res.json(["This email address already exists"]);
+        return;
+      } else {
+        console.log('4');
+        connection.query("SELECT * FROM users WHERE user_name='" + username + "'", function(err, rows, fields) {
+          if (err) {
+            console.log('2');
             console.error(err);
             res.json(["Query error"]);
             return;
-        }
-        //console.log('The database users contains: ', result);
-        console.log('The database users contains: ', rows);
-        res.send(rows);
-  });
+          } else if (rows.length > 0) {
+            console.log('3');
+            res.status(403);
+            res.json(["This username already exists"]);
+            return;
+          } else {
+            connection.query("INSERT INTO `climbing`.`users` (`user_name`, `user_password`, `user_email`, `user_role`) VALUES ('" + username + "', '" + password + "', '" + email + "', 'user')", function(err, rows, fields) {
+              if (err) {
+                console.log('5');
+                console.error(err);
+                res.json(["Query error"]);
+                return;
+              }
+            });
+            res.status(200);
+            res.json(["The user has been added to the database"]);
+          }
+        });
+      }
+    });
+
+  } else {
+    res.status(412);
+    res.json(["Incorrect parameters sent"]);
+  }
 
 });
 
