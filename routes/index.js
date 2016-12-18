dfbdb3ccce0c8521c05df4938abeffce64a7684e
unknown <anel1989@googlemail.com>
2016-12-18 13:15:36 +0000

    Started the dashboard page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',function(req,res){
+router.get('/',function(req, res){
 
     console.log("index.js function");
 
-     res.sendFile('login.html', {root: './dist'});
+    res.sendFile('login.html', {root: './dist'});
 
 });
+
+router.get('/dashboard',function(req, res){
+
+    console.log("index.js function");
+
+    res.sendFile('dashboard.html', {root: './dist'});
+
+});
+
 
 module.exports = router;
