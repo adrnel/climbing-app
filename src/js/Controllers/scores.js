5aa39b94e1bdfee7002c33ec8e7197e053ee2c13
unknown <anel1989@googlemail.com>
2017-02-19 00:51:56 +0000

    Added logic to the plus and minus buttons on the scores form

--- src/js/Controllers/scores.js
+++ src/js/Controllers/scores.js
@@ -1,4 +1,33 @@
 angular.module('climbingApp')
     .controller("scoresCtrl", ["$scope", "$location", "$http", function ($scope, $location, $http) {
+        $scope.scores={
+            spotty:0,
+            black:0,
+            tiger:0,
+            blue:0,
+            salmon:0,
+            yellow:0,
+            purpleYellow:0,
+            hendrix:0,
+            red:0,
+            white:0,
+            green:0,
+            scores:0
+        }
+        $scope.onlyNumbers = /^\d+$/;
 
+        $scope.addOne = function(colourIndex){
+            $scope.scores[Object.keys($scope.scores)[colourIndex]]++;
+        }
+        $scope.minusOne = function(colourIndex){
+            if($scope.scores[Object.keys($scope.scores)[colourIndex]] >= 1){
+                $scope.scores[Object.keys($scope.scores)[colourIndex]]--;
+            }
+        }
+        $scope.onlyNumbers = function(colourIndex){
+            $scope.scores[Object.keys($scope.scores)[colourIndex]] = String($scope.scores[Object.keys($scope.scores)[colourIndex]]).replace(/\D/g,'');
+        }
+        $scope.onlyNumbers = function(colourIndex){
+            $scope.scores[Object.keys($scope.scores)[colourIndex]] = String($scope.scores[Object.keys($scope.scores)[colourIndex]]).replace(/\D/g,'');
+        }
     }])
