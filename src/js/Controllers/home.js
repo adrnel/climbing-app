11b6c46a83763b407345cd9722236b1224fe15ed
Adrian Nelson <anel1989@googlemail.com>
2017-05-28 15:30:51 +0100

    The top nav highlights the selected route correctly

--- src/js/Controllers/home.js
+++ src/js/Controllers/home.js
@@ -1,7 +1,12 @@
 angular.module('climbingApp')
-    .controller("homeCtrl", ["$scope", "$location", "$http", function ($scope, $location, $http) {
+    .controller("homeCtrl", ["$scope", "$location", "$rootScope", function ($scope, $location, $rootScope) {
         $scope.isDropdown = false;
+
         $scope.clickMobileMenu = function(){
             $scope.isDropdown = !$scope.isDropdown;
         };
+        $scope.location = $location.path();
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $scope.location = $location.path();
+        });
     }]);
